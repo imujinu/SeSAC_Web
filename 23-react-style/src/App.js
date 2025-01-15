432cde2ed66f02c702f3e173e702219d4a224977
imujinu <wlsdnrhdwkd@gmail.com>
2025-01-15 15:26:44 +0900

    23end

--- 23-react-style/src/App.js
+++ 23-react-style/src/App.js
@@ -1,11 +1,18 @@
 import BasicCss from "./components/BasicCss";
 import ModuleCss from "./components/ModuleCss";
+import React from "./components/React";
+import Sass from "./components/Sass";
+import StyledComponents from "./components/StyledComponents";
 
 function App() {
   return (
     <div>
-      <BasicCss />
+      {/* <BasicCss />
       <ModuleCss />
+      <StyledComponents />
+       */}
+      {/* <React /> */}
+      <Sass />
     </div>
   );
 }
---
+++ 23-react-style/src/components/React.jsx
@@ -0,0 +1,55 @@
+import styled, { keyframes } from "styled-components";
+import logo from "./logo.svg";
+const RootDiv = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const spin = keyframes`
+    from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  animation: ${spin} 20s linear infinite;
+`;
+
+const MyA = styled.a`
+  color: #61dafb;
+`;
+export default function React() {
+  return (
+    <RootDiv>
+      <AppHeader>
+        <AppLogo src={logo} alt="app" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <MyA
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </MyA>
+      </AppHeader>
+    </RootDiv>
+  );
+}
